e6f622a8cac10914f7cea1d5463aa6b59513d520
Renz Edward Gallarmen <146710490+renzgallarmen@users.noreply.github.com>
2023-10-20 10:33:23 +0800

    Update COVID 19 Data Exploration.sql

--- COVID 19 Data Exploration.sql
+++ COVID 19 Data Exploration.sql
@@ -1,7 +1,7 @@
 /*
 Covid 19 Data Exploration
 
-Skills used: Joins, CTE's, Temp Tables, Window Functions, Aggregate Functions, Creating Views, Converting Data Types
+Skills used: Joins, Temp Tables, Window Functions, Aggregate Functions, Converting Data Types
 
 */
 
